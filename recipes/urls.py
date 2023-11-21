dc8e3ff994eda1a4eedf757f2dd547fb9fa25023
Rafael Araujo <rafaelaraujodasilvva@gmail.com>
2023-11-21 05:59:03 -0300

    add new file

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from recipes.views import home, sobre, contato
+
+
+urlpatterns = [
+    path('', home), #home
+    path('home/', home),#home
+    path('sobre/', sobre), #sobre
+    path('contato/', contato), #contato
+]
