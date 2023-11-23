1723f9ac8ec23ec6c4f61435b273c43ef9421a16
Rafael Araujo <rafaelaraujodasilvva@gmail.com>
2023-11-23 06:12:33 -0300

    Adicionar html da home

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
+    return render(request, 'recipes/pages/home.html')
