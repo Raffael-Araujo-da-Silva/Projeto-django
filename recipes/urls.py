f353b591ea5a937509baff1c798a2ea4f593e898
Rafael Araujo <rafaelaraujodasilvva@gmail.com>
2023-11-23 05:04:48 -0300

    Removido Contato e Sobe

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home), #home
     path('home/', home),#home
-    path('sobre/', sobre), #sobre
-    path('contato/', contato), #contato
 ]
